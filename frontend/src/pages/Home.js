0fd8931275af02412f7a91109b78dbc13d3a76bb
khawarrr <mkhawar0907@gmail.com>
2023-02-06 15:16:55 -0800

    front end home page changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+  //update local states
+  const [workouts, setWorkouts] = useState(null)
+
+  // fires a function when a componenet is rendered
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      
+      const response = await fetch('http://localhost:4000/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+
+  }, []); // [] means it'll only fire once
+
   return (
     <div className="home">
-        <h2>Home</h2>
+        <div className="workouts">
+          {workouts && workouts.map((workout) =>(
+            <p key={workout._id} >{workout.title}</p>
+          ))}
+        </div>
     </div>
   )
 }
